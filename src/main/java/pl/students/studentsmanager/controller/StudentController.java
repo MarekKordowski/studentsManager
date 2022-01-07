0d09d20f59e73d1ef374600cd6389d5a3cc4b5fa
MarekKordowski <m.kordowski21@gmail.com>
2022-01-07 23:20:14 +0100

    create student Controller

--- src/main/java/pl/students/studentsmanager/controller/StudentController.java
+++ src/main/java/pl/students/studentsmanager/controller/StudentController.java
@@ -1,8 +1,47 @@
 package pl.students.studentsmanager.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.students.studentsmanager.model.Student;
 import pl.students.studentsmanager.service.StudentService;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
 
     private StudentService studentService;
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        List<Student> students = studentService.findAllStudent();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @GetMapping("find/{id}")
+    public ResponseEntity<Student> getAllStudentsById(@PathVariable Long id) {
+        Student student = studentService.findStudentById(id);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> addEmployee(@RequestBody Student student) {
+        Student newStudent = studentService.addStudent(student);
+        return new ResponseEntity<>(newStudent, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        Student updatedStudent = studentService.updateStudent(id, student);
+        return new ResponseEntity<>(updatedStudent, HttpStatus.UPGRADE_REQUIRED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
