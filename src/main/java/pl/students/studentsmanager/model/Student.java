c01714a0347fce1dcc570a048210bc32c32237c8
MarekKordowski <m.kordowski21@gmail.com>
2022-01-07 20:32:30 +0100

    add lombook dependency to pom.xml file, and create model class Student

---
+++ src/main/java/pl/students/studentsmanager/model/Student.java
@@ -0,0 +1,23 @@
+package pl.students.studentsmanager.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+public class Student implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String imageUrl;
+
+
+}
