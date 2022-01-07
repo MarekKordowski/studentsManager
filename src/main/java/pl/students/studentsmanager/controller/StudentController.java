e0afa379c416a0973c21e01e1efdafbc1ec48025
MarekKordowski <m.kordowski21@gmail.com>
2022-01-07 22:13:34 +0100

    set student service, create exceptions class

---
+++ src/main/java/pl/students/studentsmanager/controller/StudentController.java
@@ -0,0 +1,8 @@
+package pl.students.studentsmanager.controller;
+
+import pl.students.studentsmanager.service.StudentService;
+
+public class StudentController {
+
+    private StudentService studentService;
+}
