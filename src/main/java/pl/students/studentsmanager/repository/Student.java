206912a6d8a5313c6e2f3e0286e6a3db27ac8b12
MarekKordowski <m.kordowski21@gmail.com>
2022-01-07 21:31:41 +0100

    create repository for student

---
+++ src/main/java/pl/students/studentsmanager/repository/Student.java
@@ -0,0 +1,8 @@
+package pl.students.studentsmanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Student extends JpaRepository<Student,Long> {
+
+
+}
