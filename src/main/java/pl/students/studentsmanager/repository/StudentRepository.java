af606fe12535c8c2dcf166ee3d1d4bf46e6a2372
MarekKordowski <m.kordowski21@gmail.com>
2022-01-11 11:44:48 +0100

    add condition in findByNameAndLast name method in Service, create 2 more query in repository - checking if name or lastName exist in database, refactor code in StudentController and add endpoint get students by name and lastname

--- src/main/java/pl/students/studentsmanager/repository/StudentRepository.java
+++ src/main/java/pl/students/studentsmanager/repository/StudentRepository.java
@@ -1,9 +1,22 @@
 package pl.students.studentsmanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.students.studentsmanager.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("select s from Student s where (s.name = :name) "
+            + "and  ( s.lastName =:lastName)")
+    Student findByNameAndLastName(@Param("name") String name,
+                                  @Param("lastName") String lastName);
+
+    @Query("select count(s) = 1 from Student s where s.name = ?1")
+    boolean ExistByName(String name);
+
+    @Query("select count(s) = 1 from Student s where s.lastName = ?1")
+    boolean ExistByLastName(String lastName);
 
 
 }
